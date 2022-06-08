a3277acef6ac4f1fb502e4155e40d041ada25a8d
harshitchopra7 <chopraharshit2000@gmail.com>
2022-06-08 10:44:11 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+
+// Components to be made
+// 1. Navbar
+// 2. About
+// 3. Tech Stack
+// 4. Experience
+// 5. Projects
+
+function App() {
+  return( 
+    <div className="App">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
